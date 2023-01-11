0b1937aee7df57a67afee61d13a8f57d1f3765bd
Nonsticklotion <pisit676@gmail.com>
2023-01-11 23:28:01 +0700

    add icon in nevbar

--- src/component/header/navbar.js
+++ src/component/header/navbar.js
@@ -1,24 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWrench,faDesktop } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
   return (
     <>
-      <nav class="navbar d-flex .flex-column navbar-light bg-light">
-        <div>
-          <a class="navbar-brand mr-5" href="/home">
+      <nav class="navbar d-flex flex-row navbar-light bg-light">
+        <div class="d-flex flex-row">
+          <a class="navbar-brand ml-5" href="/home">
             <img
               // src="https://res.cloudinary.com/ddrwonhdb/image/upload/v1664359314/xvxwnypzyvx3gkvooevd.jpg"
               width="30"
               height="30"
-              class="d-inline-block align-top"
+              class="d-inline-block align-top "
               alt=""
             />
             PC Part
           </a>
-          
+          <div class="m-2">
+          <FontAwesomeIcon icon={faDesktop}></FontAwesomeIcon>
+          </div>
           <a class="navbar-brand mr-5" href="/builder">
             Build your PC
           </a>
+          <div class="m-2">
+            <FontAwesomeIcon icon={faWrench}></FontAwesomeIcon>
+          </div>
           <a class="navbar-brand mr-5" href="/choosebuild">
-            PC Part
+            Browse Product
           </a>
         </div>
         <div>
