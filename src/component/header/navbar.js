e4f2c414d0e3d2c007c48a53694fae248409cab0
Nonsticklotion <pisit676@gmail.com>
2023-01-12 13:52:42 +0700

    new nevbar

--- src/component/header/navbar.js
+++ src/component/header/navbar.js
@@ -1,32 +1,51 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWrench,faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faWrench, faDesktop } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
+  const mystyle = {
+    color: "white",
+    backgroundColor: "#1A1B22",
+    fontFamily: "Arial",
+  };
+  const iconstyle = {
+    padding: "0 10px 10px 5px",
+  };
+  const padding ={
+    padding: "5px 5px 0px 5px"
+  }
+  const nevbarIcon = "d-flex flex-row";
+
   return (
     <>
-      <nav class="navbar d-flex flex-row navbar-light bg-light">
-        <div class="d-flex flex-row">
-          <a class="navbar-brand ml-5" href="/home">
-            <img
-              // src="https://res.cloudinary.com/ddrwonhdb/image/upload/v1664359314/xvxwnypzyvx3gkvooevd.jpg"
-              width="30"
-              height="30"
-              class="d-inline-block align-top "
-              alt=""
-            />
-            PC Part
+      <nav class="navbar d-flex flex-row navbar-light " style={mystyle}>
+        <div class="d-flex flex-row" style={padding}>
+          <a class="navbar-brand ml-5 d-flex flex-row" href="/home">
+            <div style={iconstyle}>
+              <img
+                // src="https://res.cloudinary.com/ddrwonhdb/image/upload/v1664359314/xvxwnypzyvx3gkvooevd.jpg"
+                width="30"
+                height="30"
+                class="d-inline-block align-top "
+                alt=""
+              />
+            </div>
+            <div class="text-warning ">PC Part</div>
           </a>
-          <div class="m-2">
-          <FontAwesomeIcon icon={faDesktop}></FontAwesomeIcon>
+          <div class={nevbarIcon}>
+            <div class="m-2">
+              <FontAwesomeIcon icon={faDesktop}></FontAwesomeIcon>
+            </div>
+            <a class="navbar-brand mr-5" href="/builder">
+              <div class="text-light">Build your PC </div>
+            </a>
           </div>
-          <a class="navbar-brand mr-5" href="/builder">
-            Build your PC
-          </a>
-          <div class="m-2">
-            <FontAwesomeIcon icon={faWrench}></FontAwesomeIcon>
+          <div class={nevbarIcon}>
+            <div class="m-2">
+              <FontAwesomeIcon icon={faWrench}></FontAwesomeIcon>
+            </div>
+            <a class="navbar-brand mr-5" href="/choosebuild">
+              <div class="text-light">Browse Product </div>
+            </a>
           </div>
-          <a class="navbar-brand mr-5" href="/choosebuild">
-            Browse Product
-          </a>
         </div>
         <div>
           <button type="button" class="btn btn-primary m-1">
